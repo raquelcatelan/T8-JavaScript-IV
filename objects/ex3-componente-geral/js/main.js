edf3cc041aab29f89c3441738eb8a973ddf47506
raquelcatelan <raquelpermarcat@outlook.com>
2019-11-06 16:29:25 -0300

    nova versaao melhorada do exercicio 3 com componentes

--- objects/ex3-componente-geral/js/main.js
+++ objects/ex3-componente-geral/js/main.js
@@ -30,11 +30,30 @@ let resultados = [
 const renderNavbar = new Navbar
 
 renderNavbar.render()
-
-document.querySelector('.cards').innerHTML = 
-resultados.map(receita => {
-    return new Card(receita).render()
+const sectionCards = document.querySelector('.cards')
+const  carregaCards = (arrayRecebido) =>{
+sectionCards.innerHTML = arrayRecebido.map(objeto => {
+    return new Card(objeto).render()
 }).join("")
+
+}  
+
+carregaCards(resultados)
+
+// document.querySelector('.cards').innerHTML = 
+// resultados.map(receita => {
+//     return new Card(receita).render()
+// }).join("")
+
+const limpar  = (value) => {
+    if(!value){
+        // console.log("Não tem valor");
+        // document.querySelector('.cards').innerHTML-resultados.map(receita=>{
+        //     return new Card(receita).render()
+        // }).join("")
+       carregaCards(resultados) 
+    }
+}
 
 document.querySelector('.button__search').addEventListener('click', function(){
     let inputValue = document.querySelector('.input__search').value.toUpperCase()
@@ -43,9 +62,10 @@ document.querySelector('.button__search').addEventListener('click', function(){
         return receita.titulo.toUpperCase().includes(inputValue) || receita.ingredientes.toUpperCase().includes(inputValue)
     })
 
-    document.querySelector('.cards').innerHTML = 
-    achados.map(encontrado => {
-        return new Card(encontrado).render()
-    }).join("")
+    // document.querySelector('.cards').innerHTML = 
+    // achados.map(encontrado => {
+    //     return new Card(encontrado).render()
+    // }).join("")
+    carregaCards(achados)
 })
 
