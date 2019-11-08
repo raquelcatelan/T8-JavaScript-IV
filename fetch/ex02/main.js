ce4957ab2687224ed83e3cbf3c8fea688ae069c2
raquelcatelan <raquelpermarcat@outlook.com>
2019-11-08 11:55:27 -0300

    terminado exercicio do post

--- fetch/ex02/main.js
+++ fetch/ex02/main.js
@@ -1,14 +1,38 @@
 const cepInput = document.getElementById("cep")
 const logradouroInput = document.getElementById("logradouro")
+const complementoInput = document.getElementById("complemento")
+const bairroInput = document.getElementById("bairro")
+const localidadeInput = document.getElementById("localidade")
+const ufInput = document.getElementById("uf")
 //... continuem a pegar os inputs do index.html para preencher depois com o valor da api
 
 cepInput.addEventListener("blur", ()=>{
-    if(cepInput.input){
+    if(cepInput.value){
         fetch(`https://viacep.com.br/ws/${cepInput.value}/json`).then(response => {
             return response.json()
+           
+            
         }).then(json => {
+            
+            preencherCampos(json)
             ///façam aparecer os valores nos inputs
+            // console.log(logradouroInput);
+            // console.log(complementoInput);
+            // console.log( bairroInput);
+            // console.log( localidadeInput);
+            // console.log(ufInput);
+        
+
 
         })
     }
 })
+
+preencherCampos = dados =>{
+    logradouroInput.value = dados.logradouro
+    complementoInput.value = dados.complemento
+    bairroInput.value = dados.bairro
+    localidadeInput.value = dados.localidade 
+    ufInput.value = dados.uf
+
+}
---
+++ fetch/ex03Post/main.js
@@ -0,0 +1,22 @@
+const titulo = document.getElementById('titulo')
+const descricao = document.getElementById('descricao')
+const btnSubmit = document.getElementById("btn-submit")
+
+btnSubmit.addEventListener('click' , function(event){
+    event.preventDefault()
+    let body  = JSON.stringify({
+        title: titulo.value,
+        body: descricao.value,
+
+        userId:1
+
+    })
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: "POST" , 
+        headers:{
+            "Content-type": "application/json"
+        },
+        body 
+
+    }).then (response => response.json()).then(json => console.log(json))
+})
